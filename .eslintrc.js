391932d5e0f1fe8a034f7f1bf0413dd3fbc3a971
Pavel <magma4810@gmail.com>
2023-11-11 18:01:23 +0300

    add pretier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,22 @@
 module.exports = {
-    env: {
-      browser: true,
-      es2021: true,
-      "jest/globals": true,
-    },
-    extends: ["eslint:recommended"],
-    parserOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-    },
-    plugins: ["jest"],
-    rules: {},
-    overrides: [
-      {
-        files: [".eslintrc.js", "babel.config.js", "jest.config.js"],
-        env: {
-          node: true, // Apply Node environment specifically for ESLint config
-        },
+  env: {
+    browser: true,
+    es2021: true,
+    "jest/globals": true,
+  },
+  extends: ["eslint:recommended", "prettier"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["jest"],
+  rules: {},
+  overrides: [
+    {
+      files: [".eslintrc.js", "babel.config.js", "jest.config.js"],
+      env: {
+        node: true, // Apply Node environment specifically for ESLint config
       },
-    ],
-  };
+    },
+  ],
+};
